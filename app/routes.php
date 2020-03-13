7ec41fb0506a7416f7800260807518e89ec31c03
Boris74000 <c.dauvier@it-students.fr>
2020-03-13 10:53:53 +0000

    first commit

---
+++ app/routes.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use App\Application\Actions\User\UpdateUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+
+return function (App $app) {
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    });
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+        $group->put('/{id}', UpdateUserAction::class);
+    });
+
+//    $app->group('/users', function (Group $group) {
+//        $group->get('', ListUsersAction::class);
+//        $group->get('/{id}', ViewUserAction::class);
+//        $group->put('/{id}', UpdateUserAction::class);
+//    });
+
+    $app->group('/jobdatings', function (Group $group) {
+        $group->get('', function(){});
+        $group->post('', function(){});
+        $group->get('/{id}', function(){});
+        $group->put('/{id}', function(){});
+        $group->delete('/{id}', function(){});
+
+        $group->post('/{id}/{listName}', function(){});
+
+        $group->get('/{id}/{listName}/{listId}', function(){});
+        $group->put('/{id}/{listName}/{listId}', function(){});
+        $group->delete('/{id}/{listName}/{listId}', function(){});
+
+
+    });
+
+
+};
---
+++ src/Application/Actions/User/UpdateUserAction.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Actions\User;
+
+use Psr\Http\Message\ResponseInterface as Response;
+
+class UpdateUserAction extends UserAction
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function action(): Response
+    {
+        $userId = (int) $this->resolveArg('id');
+
+        $datas = $this->getFormData();
+
+        $user = $this->userRepository->findUserOfId($userId);
+
+        $response = $user->update($datas);
+
+        $this->logger->info("User of id `${userId}` was updated.");
+
+        return $this->respondWithData(['status'=>$response, "user"=>$user]);
+    }
+}
